85fc3037a9c8ae64ec61081a779d419a54eb0740
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-04-16 16:17:03 +0700

    fix(models): add missing export default for Book model

--- src/models/books.js
+++ src/models/books.js
@@ -3,58 +3,60 @@ import './config/db.js';
 import sequelize from "../config/db.js";
 
 const Book = sequelize.define('Book', {
-    'id': {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: Sequelize.literal('nextval("Book")') // menggunakan sequence sesuai gambar
-    },
-    'title': {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    'author': {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    'date': {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    'category': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    'image': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    'description': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    'publisher': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    'year_published': {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    'page_count': {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    'format': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    'doi': {
-      type: DataTypes.TEXT,
-      allowNull: true
-    }
-  }, {
-    tableName: 'books',
-    timestamps: false // jika tidak ada kolom createdAt dan updatedAt
-  });
+  'id': {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: Sequelize.literal('nextval("Book")')
+  },
+  'title': {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  'author': {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  'date': {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  'category': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  'image': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  'description': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  'publisher': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  'year_published': {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  'page_count': {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  'format': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  'doi': {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+}, {
+  tableName: 'books',
+  timestamps: false
+});
+
+export default Book;
