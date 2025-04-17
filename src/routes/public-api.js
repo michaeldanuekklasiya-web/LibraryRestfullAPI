8d288082b77093e89e7b0f24cfcff187b1fd6033
Michael Danu Ekklasiya <michaeldanuekklasiya@gmail.com>
2025-04-17 11:25:14 +0700

    feat(bookmark): create route bookmark in public-api.js

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,9 +1,11 @@
 import express from "express";
 import healthController from "../controllers/health-controller.js";
 import bookController from "../controllers/book-controller.js";
+import collectionController from "../controllers/collection-controller.js";
 
 const publicRouter = new express.Router();
 
+// Books Route
 publicRouter.get("/up", healthController.ping);
 publicRouter.post("/books", bookController.uploadBook);
 publicRouter.get("/books", bookController.getAllBook);
@@ -11,4 +13,9 @@ publicRouter.get("/books/:id", bookController.getBookById);
 publicRouter.put("/books/:id", bookController.updateBook);
 publicRouter.delete("/books/:id", bookController.deleteBook);
 
+// Collection Routes
+publicRouter.post("/collections", collectionController.createCollection);
+publicRouter.get("/collections", collectionController.getAllCollections);
+publicRouter.delete("/collections/:id", collectionController.deleteCollectionById);
+publicRouter.delete("/collections", collectionController.deleteCollectionByUserAndBook);
 export { publicRouter };
